0c61f8eb642b225e8c60e7fa398039acf50dc9fc
jackmiller2708 <acekakarotpro@gmail.com>
2022-02-19 13:28:13 +0700

    Added Mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:OimXWaPJHMHmj5m3@cluster0.taaut.mongodb.net/AnimeReview',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
